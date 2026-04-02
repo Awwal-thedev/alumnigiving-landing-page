27b19ba6859b4aea3c984276cb4593065b24ce84
Awwal Adeyemo <muh-awwal.adeyemo@tech-u.edu.ng>
2026-04-02 09:46:38 +0100

    Added features section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
 import Hero from '@/components/Hero'  // Hero component
+import FeaturesSection from '@/components/features';
 
 export default function Home() {
   return (
     <main>
     <Hero />
+
+    <div className="size-full">
+      <FeaturesSection />
+    </div>
+    
   </main>
+  
   );
 }
